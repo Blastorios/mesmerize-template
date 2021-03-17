b3aa61be38fed3b89a9f5c6b6badb922a7c75bb6
Blastorios <jordi.zwiggelaar@hotmail.com>
2021-03-17 13:44:12 +0100

    Reworked overall dependecy list for testing

---
+++ {{ cookiecutter.project_name }}/src/utils/numba_configurere.py
@@ -0,0 +1,5 @@
+# TODO
+# Create a decorator from the njit decorator that allows mesmerize to see whether it needs to use parallization on a given method, based on the available resources.
+# Allowing developers to ONLY change the decorators to apply a system-wide effect!
+
+# Alternatively, make a new base-object class in mesmerize from which ALL classes inherit, allowing system-wide changes equally
